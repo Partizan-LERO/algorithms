3ffb2d15868c5a5246d45f1dac00e4c1deebb8e6
Sergey Samoylenko <corazonsergey@gmail.com>
2020-01-30 10:17:18 +0200

    Create insertionSort.js

---
+++ insertionSort.js
@@ -0,0 +1,15 @@
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+      let j = i;
+      
+      while(j > 0 && (arr[j] < arr[j-1])) {
+      	let temp = arr[j];
+        arr[j] = arr[j-1];
+        arr[j-1] = temp;
+      }
+  }
+  
+  return arr;
+}
+
+console.log(insertionSort([4, 3, 6, 15, 12, 123, 122]));
